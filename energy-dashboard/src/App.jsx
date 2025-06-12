4e7e8d2b2122fd71af43b97b82bf10943ac85f5c
kanchan-pathak <66899600+kanchan-pathak@users.noreply.github.com>
2025-06-12 11:56:22 +0000

    Hero image added

--- energy-dashboard/src/App.jsx
+++ energy-dashboard/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header'
-
+import HeroSectionImage from './assets/HeroSectionImage.jpg'
 function App() {
 
   return (
     <>
       <div>
-        <Header/>
+        <Header/>         
+<div className=" relative w-screen h-[80vh] flex justify-center items-center overflow-hidden">
+  <img
+  src={HeroSectionImage}
+  className="max-h-full w-full object-cover"
+  alt="Hero"
+ />
+<div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent"></div>
+</div>
+
+
       </div>
     </>
   )
--- energy-dashboard/src/Header.jsx
+++ energy-dashboard/src/Header.jsx
@@ -5,9 +5,9 @@ return(
     <div className="w-screen bg-foreground h-[7vh] flex justify-between">
         <LogoCard/>
         <div className= "w-96 flex items-center"><ul className="w-full flex justify-around text-primary font-bold text-lg">
-            <li className="hover:text-primaryhover">About</li>
-            <li className="hover:text-primaryhover">Home</li>
-            <li className="hover:text-primaryhover">Contact Us</li></ul></div>
+            <li className="hover:text-primaryhover-light">About</li>
+            <li className="hover:text-primaryhover-light">Home</li>
+            <li className="hover:text-primaryhover-light">Contact Us</li></ul></div>
         <PrimaryButton buttonText="Login"/>
     </div>
 )
