ae66f1fc4422ae15e542bafb66ab1616932754d9
kanchan-pathak <66899600+kanchan-pathak@users.noreply.github.com>
2025-06-12 17:11:34 +0000

    working on hero section

--- energy-dashboard/src/App.jsx
+++ energy-dashboard/src/App.jsx
@@ -8,16 +8,16 @@ function App() {
     <>
       <div>
         <Header/>         
-<div className=" relative w-screen h-[80vh] flex justify-center items-center overflow-hidden">
-  <img
-  src={HeroSectionImage}
-  className="max-h-full w-full object-cover"
-  alt="Hero"
- />
-<div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent"></div>
-</div>
-
-
+        <div className=" relative w-screen h-[80vh] flex overflow-hidden">
+        <img
+        src={HeroSectionImage}
+        className="max-h-full w-full object-cover"
+        alt="Main Image"
+        />
+        <div className="absolute inset-0 bg-gradient-to-r from-black/40 to-transparent"></div>
+        <div className="absolute left-1/4 -translate-x-1/2 top-1/2 -translate-y-1/2 p-4 w-1/3 h-1/2 rounded-2xl bg-foreground">
+        <h1 className='text-primary font-bold '>Will Add Form like control or button </h1></div>
+        </div>
       </div>
     </>
   )
